2400d15ddbead634165b1a9b9c6d998f68812011
RomilRao23 <subyadav07@gmail.com>
2024-06-17 13:37:15 +0530

    add signout functionality

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -156,6 +156,24 @@ export default function DashProfile() {
           dispatch(deleteUserFailure(error.message));
         }
       }
+
+      const handleSignout=async()=>{
+        try {
+          const res = await fetch('/api/user/signout', {
+            method: 'POST',
+          });
+          const data = await res.json();
+          if(!res.ok){
+            console.log(data.message);
+          }
+          else{
+            dispatch(signoutSuccess(data));
+          }
+        } catch (error) {
+          console.log(error.message);
+        }
+      }
+
   return (
     <div className=' w-full max-w-lg mx-auto p-3 '>
     <h1 className='my-7 text-center font-semibold text-3xl'>Profile</h1>
@@ -233,7 +251,7 @@ export default function DashProfile() {
         <span onClick={()=>setShowModal(true)} className='cursor-pointer'>
             Delete Account
         </span>
-        <span className='cursor-pointer'>
+        <span onClick={handleSignout} className='cursor-pointer'>
             Sign Out
         </span>
     </div>
@@ -254,7 +272,7 @@ export default function DashProfile() {
         {error}
       </Alert>
     )}
-    
+
     <Modal
         show={showModal}
         onClose={() => setShowModal(false)}
